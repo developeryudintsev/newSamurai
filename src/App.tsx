4b7a31ba2056942036f50f8af451e3c7c8a50379
login <kakvospitat@gmail.com>
2020-07-26 10:05:13 +0300

    26.07.2020-dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './App.css';
 import Header from "./components/Header/Herader";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Nav/>
-            <Profile/>
-
+            {/*<Profile/>*/}
+            <div className={'App-wraper-content'}>
+                <Dialogs/>
+            </div>
         </div>
     );
 }
