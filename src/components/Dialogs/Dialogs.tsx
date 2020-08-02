8b6b507046c3c9e34462a4233ece70dbee577680
login <kakvospitat@gmail.com>
2020-08-02 11:38:46 +0300

    02.08.2020-Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,17 @@ import d from './Dialogs.module.css';
 
 function Dialogs() {
     return (
-        <div>
-Dialogs
+        <div className={d.content}>
+            <div className={d.content}>
+                <div className={d.contentitem}>Sasha</div>
+                <div className={d.contentitem}>Dimych</div>
+                <div className={d.contentitem}>Igor</div>
+            </div>
+            <div className={d.messages}>
+                <div className={d.messagesmessage}>Hi</div>
+                <div className={d.messagesmessage}>Hi</div>
+                <div className={d.messagesmessage} >Hi</div>
+            </div>
         </div>
     );
 }
